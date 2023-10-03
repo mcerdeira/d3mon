851aa12a92235e4c87a2c3dbb335b6a361207038
Martin Cerdeira <martincerdeira@gmail.com>
2023-10-03 14:12:07 -0300

    Boot mejor hecho

--- BOOT.bat
+++ BOOT.bat
@@ -1,2 +1,10 @@
+echo "STARTING d3MONs SERVER version 6.6.6"
+echo off
+
+for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"') do set ip=%%b
+set ip=%ip:~1%
+echo REACT_APP_LOCAL_IP=%ip% > .\proxy-gamepad\web-gamepad\.env
+pause
+
 start "" npm --prefix .\proxy-gamepad\web-gamepad\ run start 
 start "" node .\proxy-gamepad\service\service.js
